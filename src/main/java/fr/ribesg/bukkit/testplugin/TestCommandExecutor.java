bc7f4665e92100026fde8aafdac358e2d57f2abe
Gael Ribes <ribesg@yahoo.fr>
2014-01-09 13:14:29 +0100

    Add testing stuff

--- src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
+++ src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
@@ -1,8 +1,14 @@
 package fr.ribesg.bukkit.testplugin;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 /** @author Ribesg */
@@ -14,12 +20,30 @@ public class TestCommandExecutor implements CommandExecutor {
 
 	public TestCommandExecutor(final TestPlugin plugin) {
 		this.plugin = plugin;
-		// this.plugin.getCommand("foo").setExecutor(this);
+		this.plugin.getCommand("foo").setExecutor(this);
 	}
 
 	@Override
 	public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
-		// NOP
+		if (cmd.getName().equals("foo")) {
+			if (sender instanceof Player) {
+				final Player player = (Player) sender;
+				final List<Entity> entities = player.getNearbyEntities(10.0, 10.0, 10.0);
+				for (final Entity e : entities) {
+					if (e instanceof LivingEntity) {
+						final Location from = e.getLocation();
+						e.teleport(from.clone().add(2, 3, 4));
+						player.sendMessage(ChatColor.GREEN + "Teleported a " + e.getType().name() + " randomly");
+						return true;
+					}
+				}
+				player.sendMessage(ChatColor.RED + "No living entity nearby");
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.RED + "This command is for Players");
+				return true;
+			}
+		}
 		return false;
 	}
 }
--- src/main/java/fr/ribesg/bukkit/testplugin/TestListener.java
+++ src/main/java/fr/ribesg/bukkit/testplugin/TestListener.java
@@ -1,6 +1,10 @@
 package fr.ribesg.bukkit.testplugin;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPortalEvent;
+import org.bukkit.event.entity.EntityPortalExitEvent;
+import org.bukkit.event.entity.EntityTeleportEvent;
 
 import java.util.logging.Logger;
 
@@ -14,5 +18,20 @@ public class TestListener implements Listener {
 	public TestListener(final TestPlugin plugin) {
 		this.plugin = plugin;
 		Bukkit.getPluginManager().registerEvents(this, this.plugin);
+	}
+
+	@EventHandler
+	public void onEntityTeleportEvent(final EntityTeleportEvent event) {
+		LOGGER.info(event.getEventName() + " - Cause=" + event.getCause());
+	}
+
+	@EventHandler
+	public void onEntityPortalExitEvent(final EntityPortalExitEvent event) {
+		LOGGER.info(event.getEventName() + " - Cause=" + event.getCause());
+	}
+
+	@EventHandler
+	public void onEntityPortalEvent(final EntityPortalEvent event) {
+		LOGGER.info(event.getEventName() + " - Cause=" + event.getCause());
 	}
 }
