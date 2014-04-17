ab174ca705228f4e8900ae49fa7fb868da9c4782
Ribesg <Ribesg@yahoo.fr>
2014-04-17 19:52:21 +0200

    Rework testing command

--- src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
+++ src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
@@ -1,16 +1,7 @@
 package fr.ribesg.bukkit.testplugin;
-import org.bukkit.Achievement;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.chat.AchievementHoverAction;
-import org.bukkit.chat.AchievementMessagePart;
-import org.bukkit.chat.ChatAction;
-import org.bukkit.chat.CustomMessagePart;
-import org.bukkit.chat.ItemHoverAction;
-import org.bukkit.chat.ItemMessagePart;
-import org.bukkit.chat.RichMessage;
-import org.bukkit.chat.RichMessagePart;
-import org.bukkit.chat.SuggestChatAction;
+import org.bukkit.chat.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,11 +10,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
-/** @author Ribesg */
+/**
+ * @author Ribesg
+ */
 public class TestCommandExecutor implements CommandExecutor {
 
 	private static final Logger LOGGER = Logger.getLogger(TestCommandExecutor.class.getName());
@@ -37,71 +29,63 @@ public class TestCommandExecutor implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
-		if(!(sender instanceof Player)) {
-            sender.sendMessage("Nope");
-            return true;
-        }
-        Player player = ((Player) sender);
-        if(args.length == 0) {
-            sendSample(player);
-        } else if(args[0].equals("achievements")) {
-            sendAchievements(player);
-        } else if(args[0].equals("items")) {
-            sendItems(player);
-        }
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("Nope");
+			return true;
+		}
+		Player player = ((Player) sender);
 
-        return true;
+		if (args.length < 1) {
+			return false;
+		}
+		final String arg = args[0].toLowerCase();
+		boolean all = false;
+		int value = -1;
+		if ("all".equals(arg)) {
+			all = true;
+		} else {
+			try {
+				value = Integer.parseInt(arg);
+			} catch (final NumberFormatException e) {
+				return false;
+			}
+		}
+
+		boolean spoke = false;
+
+		if (all || value == 1) {
+			player.sendRichMessage(new RichMessage("Test 1", null, null));
+			spoke = true;
+		}
+
+		if (all || value == 2) {
+			player.sendRichMessage(new RichMessage(ChatColor.RED + "Test 2", null, null));
+			spoke = true;
+		}
+
+		if (all || value == 3) {
+			player.sendRichMessage(new RichMessage(ChatColor.RED + "Test 3: ", null, null).append(new ItemMessagePart(new ItemStack(Material.GOLDEN_APPLE))));
+			spoke = true;
+		}
+
+		if (all || value == 4) {
+			final ItemStack is = new ItemStack(Material.STONE_SWORD);
+			is.setDurability((short) (Material.STONE_SWORD.getMaxDurability() / 2));
+			is.addEnchantment(Enchantment.DAMAGE_ALL, 5);
+			is.addUnsafeEnchantment(Enchantment.LOOT_BONUS_MOBS, 10);
+			final ItemMeta meta = is.getItemMeta();
+			meta.setDisplayName("" + ChatColor.RED + ChatColor.BOLD + "Red" + ChatColor.GOLD + ChatColor.BOLD + " Sword of War " + ChatColor.BOLD + ChatColor.MAGIC + "42");
+			meta.setLore(Arrays.asList(new String[] {
+					ChatColor.AQUA + "Awesomness +7",
+					ChatColor.AQUA + "Stupidity -4",
+					" ",
+					"" + ChatColor.DARK_BLUE + ChatColor.ITALIC + "Built by " + ChatColor.RED + ChatColor.MAGIC + "Ribesg"
+			}));
+			is.setItemMeta(meta);
+			player.sendRichMessage(new RichMessage(ChatColor.RED + "Test 4: ", null, null).append(new ItemMessagePart(is)));
+			spoke = true;
+		}
+
+		return spoke;
 	}
-
-    private void sendSample(Player player) {
-        RichMessage richMessage = new RichMessage();
-        ItemStack is = new ItemStack(Material.CARROT_ITEM);
-        ItemMeta meta = is.getItemMeta();
-        meta.setLore(Arrays.asList("Lore 1", ChatColor.RED + "test"));
-        meta.setDisplayName(ChatColor.BOLD + "yolo");
-        meta.addEnchant(Enchantment.ARROW_DAMAGE, 1, true);
-        is.setItemMeta(meta);
-        richMessage
-            .append(new CustomMessagePart("Click me to say 'hey' ", new SuggestChatAction("hey"), null))
-            .append(
-                new CustomMessagePart(
-                    "Click to reveal your true self ",
-                    new ChatAction("/me is gay"),
-                    new ItemHoverAction(is)
-                )
-            )
-            .append(
-                new CustomMessagePart(
-                    "This is an achievement... ",
-                    null,
-                    new AchievementHoverAction(Achievement.BAKE_CAKE)
-                )
-            );
-        player.sendRichMessage(richMessage);
-    }
-
-    private void sendAchievements(Player player) {
-        RichMessage achievs = new RichMessage();
-        for(Achievement a : Achievement.values()) {
-            achievs.append(new AchievementMessagePart(a));
-        }
-        player.sendRichMessage(achievs);
-    }
-
-    private void sendItems(Player player) {
-        RichMessage items = new RichMessage();
-        for(Material m : Material.values()) {
-            try {
-                if(m == null || m == Material.AIR || Material.class.getField(m.name()).isAnnotationPresent(Deprecated.class)) {
-                    continue;
-                }
-            } catch(NoSuchFieldException e) {
-                // IDC!
-            }
-            ItemStack is = new ItemStack(m);
-            items.append(new ItemMessagePart(is));
-        }
-        player.sendRichMessage(items);
-    }
-
 }
