52abe61169a2deb1307624e55654c9d33dc68d11
bendem <online@bendem.be>
2014-04-17 15:31:16 +0200

    send all achivements and items

--- src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
+++ src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
@@ -1,7 +1,9 @@
 package fr.ribesg.bukkit.testplugin;
 import org.bukkit.Achievement;
+import org.bukkit.Material;
 import org.bukkit.chat.AchievementHoverAction;
-import org.bukkit.chat.ClickAction;
+import org.bukkit.chat.ChatAction;
+import org.bukkit.chat.ItemHoverAction;
 import org.bukkit.chat.RichMessage;
 import org.bukkit.chat.RichMessagePart;
 import org.bukkit.chat.SuggestChatAction;
@@ -9,6 +11,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.logging.Logger;
 
@@ -31,19 +34,41 @@ public class TestCommandExecutor implements CommandExecutor {
             return true;
         }
         Player player = ((Player) sender);
-        RichMessage richMessage = new RichMessage();
-        richMessage
-                .append(new RichMessagePart("Click me to say 'hey'", new SuggestChatAction("hey"), null))
-                .append(" ")
-                .append(
-                    new RichMessagePart(
-                        "Hover to know what you see when you get iron'",
-                        null,
-                        new AchievementHoverAction(Achievement.ACQUIRE_IRON)
-                    )
-                );
+        if(args.length == 0) {
+            sendSample(player);
+        } else if(args[0].equals("achievements")) {
+            sendAchievements(player);
+        } else if(args[0].equals("items")) {
+            sendItems(player);
+        }
 
-        player.sendRichMessage(richMessage);
         return true;
 	}
+
+    private void sendSample(Player player) {
+        RichMessage richMessage = new RichMessage();
+        richMessage
+            .append(new RichMessagePart("Click me to say 'hey'", new SuggestChatAction("hey"), null))
+            .append(
+                    new RichMessagePart("Click to reveal your true self", new ChatAction("/me is gay"), new ItemHoverAction(new ItemStack(Material.CARROT_ITEM)))
+            );
+        player.sendRichMessage(richMessage);
+    }
+
+    private void sendAchievements(Player player) {
+        RichMessage achievs = new RichMessage();
+        for(Achievement a : Achievement.values()) {
+            achievs.append(new RichMessagePart(a.name(), null, new AchievementHoverAction(a)));
+        }
+        player.sendRichMessage(achievs);
+    }
+
+    private void sendItems(Player player) {
+        RichMessage items = new RichMessage();
+        for(Material m : Material.values()) {
+            items.append(new RichMessagePart(m.name(), null, new ItemHoverAction(new ItemStack(m))));
+        }
+        player.sendRichMessage(items);
+    }
+
 }
