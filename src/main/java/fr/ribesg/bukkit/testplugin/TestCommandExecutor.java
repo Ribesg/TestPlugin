8f94fbefe109f27b5965777f23aff58103389335
bendem <online@bendem.be>
2014-04-17 15:12:47 +0200

    Change bukkit version + add command executor

--- src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
+++ src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
@@ -1,7 +1,14 @@
 package fr.ribesg.bukkit.testplugin;
+import org.bukkit.Achievement;
+import org.bukkit.chat.AchievementHoverAction;
+import org.bukkit.chat.ClickAction;
+import org.bukkit.chat.RichMessage;
+import org.bukkit.chat.RichMessagePart;
+import org.bukkit.chat.SuggestChatAction;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.logging.Logger;
 
@@ -14,12 +21,29 @@ public class TestCommandExecutor implements CommandExecutor {
 
 	public TestCommandExecutor(final TestPlugin plugin) {
 		this.plugin = plugin;
-		// this.plugin.getCommand("foo").setExecutor(this);
+		this.plugin.getCommand("foo").setExecutor(this);
 	}
 
 	@Override
 	public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
-		// NOP
-		return false;
+		if(!(sender instanceof Player)) {
+            sender.sendMessage("Nope");
+            return true;
+        }
+        Player player = ((Player) sender);
+        RichMessage richMessage = new RichMessage();
+        richMessage
+                .append(new RichMessagePart("Click me to say 'hey'", new SuggestChatAction("hey"), null))
+                .append(" ")
+                .append(
+                    new RichMessagePart(
+                        "Hover to know what you see when you get iron'",
+                        null,
+                        new AchievementHoverAction(Achievement.ACQUIRE_IRON)
+                    )
+                );
+
+        player.sendRichMessage(richMessage);
+        return true;
 	}
 }
