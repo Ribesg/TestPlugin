835e8d32416d9c205a98cd94387444fd75856a4a
bendem <online@bendem.be>
2014-04-17 18:18:43 +0200

    Mirror bukkit changes and add Material checks

--- src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
+++ src/main/java/fr/ribesg/bukkit/testplugin/TestCommandExecutor.java
@@ -1,18 +1,26 @@
 package fr.ribesg.bukkit.testplugin;
 import org.bukkit.Achievement;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.chat.AchievementHoverAction;
+import org.bukkit.chat.AchievementMessagePart;
 import org.bukkit.chat.ChatAction;
+import org.bukkit.chat.CustomMessagePart;
 import org.bukkit.chat.ItemHoverAction;
+import org.bukkit.chat.ItemMessagePart;
 import org.bukkit.chat.RichMessage;
 import org.bukkit.chat.RichMessagePart;
 import org.bukkit.chat.SuggestChatAction;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 /** @author Ribesg */
@@ -47,10 +55,27 @@ public class TestCommandExecutor implements CommandExecutor {
 
     private void sendSample(Player player) {
         RichMessage richMessage = new RichMessage();
+        ItemStack is = new ItemStack(Material.CARROT_ITEM);
+        ItemMeta meta = is.getItemMeta();
+        meta.setLore(Arrays.asList("Lore 1", ChatColor.RED + "test"));
+        meta.setDisplayName(ChatColor.BOLD + "yolo");
+        meta.addEnchant(Enchantment.ARROW_DAMAGE, 1, true);
+        is.setItemMeta(meta);
         richMessage
-            .append(new RichMessagePart("Click me to say 'hey'", new SuggestChatAction("hey"), null))
+            .append(new CustomMessagePart("Click me to say 'hey' ", new SuggestChatAction("hey"), null))
             .append(
-                    new RichMessagePart("Click to reveal your true self", new ChatAction("/me is gay"), new ItemHoverAction(new ItemStack(Material.CARROT_ITEM)))
+                new CustomMessagePart(
+                    "Click to reveal your true self ",
+                    new ChatAction("/me is gay"),
+                    new ItemHoverAction(is)
+                )
+            )
+            .append(
+                new CustomMessagePart(
+                    "This is an achievement... ",
+                    null,
+                    new AchievementHoverAction(Achievement.BAKE_CAKE)
+                )
             );
         player.sendRichMessage(richMessage);
     }
@@ -58,7 +83,7 @@ public class TestCommandExecutor implements CommandExecutor {
     private void sendAchievements(Player player) {
         RichMessage achievs = new RichMessage();
         for(Achievement a : Achievement.values()) {
-            achievs.append(new RichMessagePart(a.name() + " / ", null, new AchievementHoverAction(a)));
+            achievs.append(new AchievementMessagePart(a));
         }
         player.sendRichMessage(achievs);
     }
@@ -66,7 +91,15 @@ public class TestCommandExecutor implements CommandExecutor {
     private void sendItems(Player player) {
         RichMessage items = new RichMessage();
         for(Material m : Material.values()) {
-            items.append(new RichMessagePart(m.name() + " / ", null, new ItemHoverAction(new ItemStack(m))));
+            try {
+                if(m == null || m == Material.AIR || Material.class.getField(m.name()).isAnnotationPresent(Deprecated.class)) {
+                    continue;
+                }
+            } catch(NoSuchFieldException e) {
+                // IDC!
+            }
+            ItemStack is = new ItemStack(m);
+            items.append(new ItemMessagePart(is));
         }
         player.sendRichMessage(items);
     }
